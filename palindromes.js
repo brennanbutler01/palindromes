6a261dcd95dd6be88b0fe3518cfaa1cb6f0813b1
Brennan <brennanbutler01@gmail.com>
2020-06-06 15:05:53 -0700

    removed duplicated replace method - called once instead of twice

--- palindromes.js
+++ palindromes.js
@@ -1,12 +1,10 @@
 function palindromes(string) {
+    string=string.replace(/[^a-zA-Z]/g,""); //remove anything that is not alphabetical
     var newString = ''; // create a new string - this will hold the reversed string
-  
 
     for (let i = string.length-1; i>=0; i--) { //decrement - start at last element of string
         newString +=string[i]; // append the char @ index to the newString
     }
-newString=newString.replace(/[^a-zA-Z]/g,"");
-string=string.replace(/[^a-zA-Z]/g,"");
 
 if (string.toLowerCase()===newString.toLowerCase()) { // check to see if they are equal
     return`PALINDROME`; // if yes then you will return palindrome
